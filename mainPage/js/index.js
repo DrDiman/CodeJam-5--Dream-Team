f48e490d854cf46b6d5da64435f1c880b113ffbe
Dzmitry <35468938+DrDiman@users.noreply.github.com>
2019-01-25 17:59:51 +0300

    feat: add template of the main page

---
+++ mainPage/js/index.js
@@ -0,0 +1,23 @@
+
+const lang = document.querySelector('.language-switch');
+lang.addEventListener('click', chooseLang);
+
+function chooseLang(e) {
+  let body = document.body;
+  if (e.target.classList.contains('rus')) {
+    e.target.classList.toggle('rus');
+    e.target.classList.toggle('eng');
+    e.target.textContent = 'English';
+    body.className = 'en';
+  } else if (e.target.classList.contains('eng')) {
+    e.target.classList.toggle('eng');
+    e.target.classList.toggle('bel');
+    e.target.textContent = 'Беларуская';
+    body.className = 'by';
+  } else if (e.target.classList.contains('bel')) {
+    e.target.classList.toggle('bel');
+    e.target.classList.toggle('rus');
+    e.target.textContent = 'Русский';
+    body.className = 'ru';
+  }
+}
