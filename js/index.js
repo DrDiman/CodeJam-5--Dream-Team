5e1744835454126e3fab52e84c2dd544e0a90cb8
Boris Siropchik <bsiropchik@gmail.com>
2019-01-27 00:07:17 +0300

    fix: language switcher

--- js/index.js
+++ js/index.js
@@ -1,23 +1,28 @@
 
 const lang = document.querySelector('.language-switch');
+document.body.className = (
+  localStorage.getItem('currentLang') == 'eng' ? 'en' : 
+    localStorage.getItem('currentLang') == 'bel' ? 'by' : 'ru'
+)
+lang.className = (
+  localStorage.getItem('currentLang') == 'eng' ? 'language-switch eng' : 
+    localStorage.getItem('currentLang') == 'bel' ? 'language-switch bel' : 'language-switch rus'
+)
 lang.addEventListener('click', chooseLang);
 
 function chooseLang(e) {
   let body = document.body;
   if (e.target.classList.contains('rus')) {
-    e.target.classList.toggle('rus');
-    e.target.classList.toggle('eng');
-    e.target.textContent = 'English';
+    localStorage.setItem('currentLang', 'eng');
+    e.target.parentNode.className = "language-switch eng";
     body.className = 'en';
   } else if (e.target.classList.contains('eng')) {
-    e.target.classList.toggle('eng');
-    e.target.classList.toggle('bel');
-    e.target.textContent = 'Беларуская';
+    localStorage.setItem('currentLang', 'bel');
+    e.target.parentNode.className = "language-switch bel"
     body.className = 'by';
   } else if (e.target.classList.contains('bel')) {
-    e.target.classList.toggle('bel');
-    e.target.classList.toggle('rus');
-    e.target.textContent = 'Русский';
+    localStorage.setItem('currentLang', 'rus');
+    e.target.parentNode.className = "language-switch rus"
     body.className = 'ru';
   }
 }
