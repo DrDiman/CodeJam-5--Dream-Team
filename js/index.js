71167d149a6f7021a63e714711df7953ae4bf536
Boris Siropchik <bsiropchik@gmail.com>
2019-01-27 01:09:08 +0300

    fix: list page layout

--- js/index.js
+++ js/index.js
@@ -25,19 +25,21 @@ function chooseLang(e) {
     localStorage.setItem('currentLang', 'eng');
     e.target.parentNode.className = "language-switch eng";
     body.className = 'en';
-    changeAuthorOfdayLang('en');
+    //changeAuthorOfdayLang('en');
     initSearch('eng');
   } else if (e.target.classList.contains('eng')) {
     localStorage.setItem('currentLang', 'bel');
     e.target.parentNode.className = "language-switch bel"
     body.className = 'by';
-    changeAuthorOfdayLang('by');
+    //changeAuthorOfdayLang('by');
     initSearch('bel');
   } else if (e.target.classList.contains('bel')) {
     localStorage.setItem('currentLang', 'rus');
     e.target.parentNode.className = "language-switch rus"
     body.className = 'ru';
-    changeAuthorOfdayLang('ru');
+    //changeAuthorOfdayLang('ru');
     initSearch('rus');
   }
 }
+
+
