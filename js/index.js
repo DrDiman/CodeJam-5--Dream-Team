f718b36e51ce6076d1b079056c9089f82708acaa
Dzmitry <35468938+DrDiman@users.noreply.github.com>
2019-01-26 17:19:31 +0300

    fix: fix bug with the only one language for searching

--- js/index.js
+++ js/index.js
@@ -1,6 +1,15 @@
-
 const lang = document.querySelector('.language-switch');
 lang.addEventListener('click', chooseLang);
+
+function initSearch(target) {
+  if (document.querySelector('.search')) {
+    const options = {
+      valueNames: [target, 'card-summary'],
+    };
+    return new List('cards', options);
+  }
+}
+initSearch('rus');
 
 function chooseLang(e) {
   let body = document.body;
@@ -9,15 +18,18 @@ function chooseLang(e) {
     e.target.classList.toggle('eng');
     e.target.textContent = 'English';
     body.className = 'en';
+    initSearch('eng');
   } else if (e.target.classList.contains('eng')) {
     e.target.classList.toggle('eng');
     e.target.classList.toggle('bel');
     e.target.textContent = 'Беларуская';
     body.className = 'by';
+    initSearch('bel');
   } else if (e.target.classList.contains('bel')) {
     e.target.classList.toggle('bel');
     e.target.classList.toggle('rus');
     e.target.textContent = 'Русский';
     body.className = 'ru';
+    initSearch('rus');
   }
 }
