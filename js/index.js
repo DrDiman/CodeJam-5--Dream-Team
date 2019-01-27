50a32d7f676769f50a03303035649d23edc27228
Boris Siropchik <bsiropchik@gmail.com>
2019-01-27 17:58:36 +0300

    feat: add Dobrolubov page

--- js/index.js
+++ js/index.js
@@ -1,4 +1,9 @@
+if(!localStorage.getItem('currentLang')){
+  localStorage.setItem('currentLang', 'rus');
+}
+
 const lang = document.querySelector('.language-switch');
+
 document.body.className = (
   localStorage.getItem('currentLang') == 'eng' ? 'en' :
     localStorage.getItem('currentLang') == 'bel' ? 'by' : 'ru'
@@ -7,6 +12,7 @@ lang.className = (
   localStorage.getItem('currentLang') == 'eng' ? 'language-switch eng' :
     localStorage.getItem('currentLang') == 'bel' ? 'language-switch bel' : 'language-switch rus'
 )
+
 lang.addEventListener('click', chooseLang);
 
 function initSearch() {
@@ -25,19 +31,19 @@ function chooseLang(e) {
     localStorage.setItem('currentLang', 'eng');
     e.target.parentNode.className = "language-switch eng";
     body.className = 'en';
-    //changeAuthorOfdayLang('en');
+    changeAuthorOfdayLang('eng');
     initSearch();
   } else if (e.target.classList.contains('eng')) {
     localStorage.setItem('currentLang', 'bel');
     e.target.parentNode.className = "language-switch bel"
     body.className = 'by';
-    //changeAuthorOfdayLang('by');
+    changeAuthorOfdayLang('bel');
     initSearch();
   } else if (e.target.classList.contains('bel')) {
     localStorage.setItem('currentLang', 'rus');
     e.target.parentNode.className = "language-switch rus"
     body.className = 'ru';
-    //changeAuthorOfdayLang('ru');
+    changeAuthorOfdayLang('rus');
     initSearch();
   }
 }
