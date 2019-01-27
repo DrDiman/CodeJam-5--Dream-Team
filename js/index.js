c3abf38b1e1f735cd422bba418c34e967d45c4a3
Dzmitry <35468938+DrDiman@users.noreply.github.com>
2019-01-28 01:19:56 +0300

    refactor: add ptashuk & dobrolyubov

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-if(!localStorage.getItem('currentLang')){
+if (!localStorage.getItem('currentLang')) {
   localStorage.setItem('currentLang', 'rus');
 }
 
@@ -29,19 +29,19 @@ function chooseLang(e) {
   let body = document.body;
   if (e.target.classList.contains('rus')) {
     localStorage.setItem('currentLang', 'eng');
-    e.target.parentNode.className = "language-switch eng";
+    e.target.parentNode.className = 'language-switch eng';
     body.className = 'en';
     changeAuthorOfdayLang('eng');
     initSearch();
   } else if (e.target.classList.contains('eng')) {
     localStorage.setItem('currentLang', 'bel');
-    e.target.parentNode.className = "language-switch bel"
+    e.target.parentNode.className = 'language-switch bel';
     body.className = 'by';
     changeAuthorOfdayLang('bel');
     initSearch();
   } else if (e.target.classList.contains('bel')) {
     localStorage.setItem('currentLang', 'rus');
-    e.target.parentNode.className = "language-switch rus"
+    e.target.parentNode.className = 'language-switch rus';
     body.className = 'ru';
     changeAuthorOfdayLang('rus');
     initSearch();
@@ -58,7 +58,9 @@ const directorsListRu = [
   'Лобач Артем Александрович',
   'Аршанский Эдуард Львович',
   'Белоусов Олег Павлович',
-  'Иосиф Наумовоич Вейнярович'
+  'Иосиф Наумовоич Вейнярович',
+  'Игорь Михайлович Добролюбов',
+  'Пташук Михаил Николаевич',
 ];
 
 const directorsListBy = [
@@ -66,51 +68,64 @@ const directorsListBy = [
   'Лобач Арцем Аляксандравіч',
   'Аршанскі Эдуард Львовіч',
   'Белавусаў Алег Паўлавіч',
-  'Вейняровіч Іосіф Навумавіч'
+  'Вейняровіч Іосіф Навумавіч',
+  'Ігар Міхайлавіч Дабралюбаў',
+  'Пташук Міхаіл Мікалаевіч',
 ];
 
 const directorsListEn = [
-  'Orlov Vladimir Alexandrovich',
-  'Lobach Artem Aleksandrovich',
-  'Arshansky Edward Lvovich',
-  'Belousov Oleg Pavlovich',
-  'Veynyarovich Iosif Naumovoich'
-]
+  'Vladimir Orlov',
+  'Artem Lobach',
+  'Edward Arshansky',
+  'Oleg Belousov',
+  'Iosif Veynyarovich',
+  'Igor Dobrolyubov',
+  'Mikhail Ptashuk',
+];
 
 const directorsImgs = [
   'orlov000',
   'A-lobach',
   'Arshanski',
   'Belous',
-  'Veinerovich'
-]
+  'Veinerovich',
+  'dobrolyubov000',
+  'ptashuk1',
+];
 
 const directorsLinks = [
   'orlov.html',
   'lobach.html',
   'arshanski.html',
   'belousov.html',
-  'veinerovich.html'
-]
+  'veinerovich.html',
+  'dobrolyubov.html',
+  'ptashuk.html',
+];
 
 const random_index = Math.floor(Math.random() * directorsListRu.length);
 const director = document.querySelector('.random_director-title');
 const directorImg = document.querySelector('.random img');
 const directorLink = document.querySelector('.random a');
 
-if(director)director.textContent = directorsListRu[random_index];
-if(directorImg)directorImg.setAttribute('src', `${pathToImages}${directorsImgs[random_index]}.png`);
-if(directorLink)directorLink.setAttribute('href', `${pathToPages}${directorsLinks[random_index]}`);
+if (director) director.textContent = directorsListRu[random_index];
+if (directorImg) directorImg.setAttribute('src', `${pathToImages}${directorsImgs[random_index]}.jpg`);
+if (directorLink) directorLink.setAttribute('href', `${pathToPages}${directorsLinks[random_index]}`);
 
-function changeAuthorOfdayLang(lang){
-  if(director){
+function changeAuthorOfdayLang(lang) {
+  if (director) {
     switch (lang) {
-      case 'rus': director.textContent = directorsListRu[random_index]; break;
-      case 'bel': director.textContent = directorsListBy[random_index]; break;
-      case 'eng': director.textContent = directorsListEn[random_index]; break;
+      case 'rus':
+        director.textContent = directorsListRu[random_index];
+        break;
+      case 'bel':
+        director.textContent = directorsListBy[random_index];
+        break;
+      case 'eng':
+        director.textContent = directorsListEn[random_index];
+        break;
     }
   }
 }
 
-changeAuthorOfdayLang(localStorage.getItem('currentLang'))
-
+changeAuthorOfdayLang(localStorage.getItem('currentLang'));
